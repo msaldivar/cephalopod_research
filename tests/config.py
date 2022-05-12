12f71a2c6c8b442d57e75e325b32f798f649baf7
root <root@force.localdomain>
2022-05-12 17:13:33 -0600

    Add more info to config.py and new tests
    * Add headers and request object to config.py
    * Add test_list_all_markets
    * Add test_list_markets_limit
    * Add test_list_markets_limit_errors
    * Add test_details_of_market

--- tests/config.py
+++ tests/config.py
@@ -1,5 +1,11 @@
+import requests
 # Replace the URL below
 API_BASE_URL = 'https://api.cryptowat.ch/'
 API_KEY = 'replace_me'
 
+headers = {
+    "Accept": "application/json",
+    "X-CW-API-Key": API_KEY,
+}
 
+cw_api = requests.Session()
--- tests/markets/test_markets_api.py
+++ tests/markets/test_markets_api.py
@@ -1,6 +1,95 @@
 from unittest import TestCase
 
+from cephalopod_research.tests import config
+from cephalopod_research.tests.config import cw_api
+
 
 class TestListMarkets(TestCase):
     """Test class for /markets endpoint"""
+
+    def test_list_all_markets(self):
+        """Check return of /markets endpoint returns all markets for all exchanges."""
+
+        url = config.API_BASE_URL + 'markets'
+        response = cw_api.get(
+            url,
+            headers=config.headers,
+            verify=True,
+            timeout=(5, 20),
+            allow_redirects=True,
+        )
+        rjson = response.json()
+
+        assert response.status_code == 200
+        assert rjson['result']
+
+        # assert cursor has values
+        assert 'last' in rjson['cursor']
+        assert 'hasMore' in rjson['cursor']
+
+        # assert allowance has values
+        assert 'cost' in rjson['allowance']
+        assert 'remaining' in rjson['allowance']
+        assert 'remainingPaid' in rjson['allowance']
+        assert 'account' in rjson['allowance']
+
+        # assert an individual result has values
+        assert 'id' in rjson['result'][0]
+        assert 'exchange' in rjson['result'][0]
+        assert 'pair' in rjson['result'][0]
+        assert 'active' in rjson['result'][0]
+        assert 'route' in rjson['result'][0]
+
+    def test_list_markets_limit(self):
+        """Check return of /markets?limit=2 endpoint returns all markets for all exchanges."""
+
+        url = config.API_BASE_URL + 'markets?limit=2'
+        response = cw_api.get(
+            url,
+            headers=config.headers,
+            verify=True,
+            timeout=(5, 20),
+            allow_redirects=True,
+        )
+        rjson = response.json()
+
+        assert response.status_code == 200
+        assert len(rjson['result']) == 2
+
+        for i in range(len(rjson['result'])):
+            assert 'id' in rjson['result'][i]
+            assert 'exchange' in rjson['result'][i]
+            assert 'pair' in rjson['result'][i]
+            assert 'active' in rjson['result'][i]
+            assert 'route' in rjson['result'][i]
+
+    def test_list_markets_limit_errors(self):
+        """Check return of /markets?limit forcing errors."""
+
+        # limit should only work between [1-20000]
+
+        # test negative limit - 400 Bad Request
+        url = config.API_BASE_URL + 'markets?limit=-1'
+        response = cw_api.get(
+            url,
+            headers=config.headers,
+            verify=True,
+            timeout=(5, 20),
+            allow_redirects=True,
+        )
+        assert response.status_code == 400
+
+        url = config.API_BASE_URL + 'markets?limit=abc'
+        response = cw_api.get(
+            url,
+            headers=config.headers,
+            verify=True,
+            timeout=(5, 20),
+            allow_redirects=True,
+        )
+        assert response.status_code == 400
+
+    def test_details_of_market(self):
+        """"""
+
 
