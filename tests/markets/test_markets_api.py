f77c1dc5d3152a2ac27fac76197240a60feb6156
root <root@force.localdomain>
2022-05-12 15:17:56 -0600

    Add several test cases
    * Add test_list_all_markets_return
    * Add test_list_exchange
    * Add test_list_exchange_error

--- tests/markets/test_markets_api.py
+++ tests/markets/test_markets_api.py
@@ -1,6 +1,34 @@
 from unittest import TestCase
+import pytest
+import cryptowatch as cw
+from datetime import datetime
 
 
 class TestListMarkets(TestCase):
-    """Test class for /markets endpoint"""
+    """Test class for /markets endpoint."""
 
+    def test_list_all_markets_return(self):
+        """Check return of /markets endpoint returns all markets for all exchanges."""
+
+        # make api call to /markets
+        markets = cw.markets.list()
+
+        assert markets._http_response.status_code == 200
+        assert type(markets.markets) == type(list())
+
+    def test_list_exchange(self):
+        """Check return of /markets?exchanges=kraken."""
+
+        # make api call to /markets?exchanges=kraken
+        markets = cw.markets.list(exchange='kraken')
+
+        assert markets._http_response.status_code == 200
+
+        assert markets.markets[0].exchange == 'kraken'
+
+    def test_list_exchange_error(self):
+        """Pass a nonexistent exhange to check error"""
+
+        # make api call to /markets?exchanges=cephalopod
+        with pytest.raises(cw.errors.APIResourceNotFoundError):
+            markets = cw.markets.list(exchange='cephalopod')
