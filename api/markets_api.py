17dfcba9500ea689039546b59a610ab02ba3733d
root <root@force.localdomain>
2022-05-14 15:15:42 -0600

    Add basic markets api wrapper
    * Basic markets api wrapper introduced making testing easier

---
+++ api/markets_api.py
@@ -0,0 +1,32 @@
+import requests
+from cephalopod_research.tests import config
+
+
+class CryptowatchMarketsAPIWrapper:
+    """A simple wrapper for the cryptowatch /markets endpoint"""
+
+    def __init__(self):
+        self.url = config.API_BASE_URL
+        self.cw_api = requests.Session()
+
+        if config.API_KEY == 'replace_me':
+            raise ValueError('Add api key to config.py')
+
+        self.headers = {
+            "Accept": "application/json",
+            "X-CW-API-Key": config.API_KEY,
+        }
+
+    def get(self, params=''):
+        """Perform the request.get call based on the passed in params"""
+
+        url = self.url + params
+        response = self.cw_api.get(
+            url,
+            headers=self.headers,
+            verify=True,
+            timeout=(5, 20),
+            allow_redirects=True,
+        )
+
+        return response
