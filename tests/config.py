695eed4da8bf834e21166a318e058d6e29509fef
root <root@force.localdomain>
2022-05-14 15:16:30 -0600

    Cleanup config file
    * remove package imports and global decloration of request object
    * remove headers and reformat

--- tests/config.py
+++ tests/config.py
@@ -1,11 +1,2 @@
-import requests
-# Replace the URL below
 API_BASE_URL = 'https://api.cryptowat.ch/'
 API_KEY = 'replace_me'
-
-headers = {
-    "Accept": "application/json",
-    "X-CW-API-Key": API_KEY,
-}
-
-cw_api = requests.Session()
